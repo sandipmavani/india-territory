0266195caeb5237f07901f03b543990e8ff99e61
sandip.mavani <sandip.mavani@avinashi.com>
2017-05-01 18:36:22 +0530

    change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var Territory     = require('./models/Territory');
 var _             = require('underscore');
 var async         = require('async');
 
-exports.searchFromPINCode = (pincode, callback) => {
+/*exports.searchFromPINCode = (pincode, callback) => {
     database  = db;
     var res   = [];
     var query = {
@@ -28,6 +28,27 @@ exports.searchFromPINCode = (pincode, callback) => {
             callback(_.chain(res).sortBy('city').sortBy('pincode').value());
         });
     });
+    }).catch(err => {
+        callback(err);
+});
+};*/
+
+exports.searchFromPINCode = (pincode, callback) => {
+    connect(uriConnection).then(function(db) {
+        database  = db;
+        var res   = [];
+        var query = {
+            pincode : new RegExp(pincode, 'i')
+        };
+
+        Territory.find(query, { populate : false }).then(function(p) {
+            async.eachOf(p, function(value, key, cb) {
+                res.push(_.pick(value, 'pincode', 'city'));
+                cb();
+            }, function(err) {
+                callback(_.sortBy(res, 'city'));
+            });
+        });
     }).catch(err => {
         callback(err);
 });
